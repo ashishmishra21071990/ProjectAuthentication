46bb1a637c3c6124580495d5058f6c1b7f8a1c62
ashish1956 <ashishmishra1956@gmail.com>
2024-07-16 13:18:16 +0530

    second commit

---
+++ AuthenticationMVCCore/AuthenticationMVCCore/Controllers/AccountController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AuthenticationMVCCore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string userName,string Password)
+        {
+            if (string.IsNullOrEmpty(userName) && string.IsNullOrEmpty(Password))
+            {
+                return RedirectToAction("Login");
+            }
+            // Check the UserName and Password
+            // Implement Cookie Authentication Logic
+            ClaimsIdentity identity = null;
+            bool isAuthenticated = false;
+            if (userName == "Mohit" && Password == "password")
+            {
+                // Create the identity for Admin
+                identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name,userName),
+                    new Claim(ClaimTypes.Role,"Admin")
+                }, CookieAuthenticationDefaults.AuthenticationScheme);
+                isAuthenticated = true;
+            }
+            if (userName == "Ashish" && Password == "mishra")
+            {
+                //Create the identity for User
+                identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name,userName),
+                    new Claim(ClaimTypes.Role,"Manager")
+                }, CookieAuthenticationDefaults.AuthenticationScheme);
+                isAuthenticated = true;
+            }
+            if (userName == "Prateek" && Password == "kumar")
+            {
+                //Create the identity for User
+                identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name,userName),
+                    new Claim(ClaimTypes.Role,"User")
+                }, CookieAuthenticationDefaults.AuthenticationScheme);
+                isAuthenticated = true;
+            }
+
+            if (isAuthenticated)
+            {
+                var principal = new ClaimsPrincipal(identity);
+                var login = HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+                return RedirectToAction("Index", "AuthSecurity");
+            }
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            var login = HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ AuthenticationMVCCore/AuthenticationMVCCore/Controllers/AuthSecurityController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationMVCCore.Controllers
+{
+    // Implementing Authorization
+
+    [Authorize(Roles = "Admin,Manager")]
+    public class AuthSecurityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize(Roles ="Admin")]
+        public IActionResult Settings()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult About()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
